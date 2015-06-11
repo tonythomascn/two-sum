0eae6ae88b922cf69b93db3dea69282f501fa60b
Tony Liu <tonythomascn@gmail.com>
2015-06-11 14:01:42 -0400

    Given an array of integers, find two numbers such that they add up to a specific target number.
    The function twoSum should return indices of the two numbers such that
    they add up to the target, where index1 must be less than index2.
    Please note that your returned answers (both index1 and index2) are
    not zero-based.
    
    You may assume that each input would have exactly one solution.
    
    Input: numbers={2, 7, 11, 15}, target=9
    Output: index1=1, index2=2
    
    Solution in C&C++

---
+++ two sum/two sum.cpp
@@ -0,0 +1,64 @@
+// two sum.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <unordered_map>
+using std::vector;
+using std::unordered_map;
+
+vector<int> twoSum(vector<int>& nums, int target);
+int * twoSum(int* nums, int numsSize, int target);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	vector<int> nums{ 0,2,4,6,8,10,12,14,16,18,20,22,24 };
+	vector<int> index = twoSum(nums, 6);
+	for each (int i in index)
+		printf(" %d", i);
+	system("pause");
+	return 0;
+}
+vector<int> twoSum(vector<int>& nums, int target)
+{
+	vector<int> indexs(2);
+	unordered_map<int, int> mp;
+	for (int i = 0; i < nums.size(); i++){
+			if (mp.find(target - nums[i]) != mp.end()) {
+				indexs[0] = mp[target - nums[i]] + 1;
+				indexs[1] = i + 1;
+				return indexs;
+			}
+			mp[nums[i]] = i;
+	}
+	return indexs;
+}
+vector<int> twoSum(vector<int>& nums, int target)
+{
+	vector<int> indexs(2);
+	for (int i = 0; i < nums.size(); i++) {
+		for (int j = i - 1; j >= 0; j--)
+		if (target - nums[i] == nums[j]) {
+			indexs[0] = j + 1;
+			indexs[1] = i + 1;
+			return indexs;
+		}
+	}
+	return indexs;
+}
+int * twoSum(int* nums, int numsSize, int target)
+{
+	int *index = (int*)malloc(sizeof(int)*2);
+	int i, j;
+	for (i = numsSize - 1; i >= 0; i--) {
+		if (nums[i] <= target || target < 0)
+		for (j = i - 1; j >= 0; j--) {
+			if (target - nums[i] == nums[j]) {
+				index[0] = j + 1;
+				index[1] = i + 1;
+				return index;
+			}
+		}
+	}
+	return index;
+}
