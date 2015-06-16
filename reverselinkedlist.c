199650f5784ea12ed22c5997201fea051841b268
Tony Liu <tonythomascn@gmail.com>
2015-06-16 11:47:48 -0400

    reverse for singly linked list in pure c

---
+++ reverselinkedlist.c
@@ -0,0 +1,55 @@
+
+ //Definition for singly-linked list.
+ struct ListNode {
+   int val;
+     struct ListNode *next;
+ };
+
+#include <stdlib.h>
+#include <stdio.h>
+struct ListNode* reverseList(struct ListNode* head) {
+	struct ListNode *curr = head;
+	struct ListNode *next = NULL;
+	struct ListNode *prev = NULL;
+
+	while (curr) {
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+		head = prev;
+	}
+
+	return head;
+}
+void push(struct ListNode *head, int val) {
+	struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+	node->val = val;
+	node->next = NULL;
+	while (head->next) {
+		head = head->next;
+	}
+	head->next = node;
+	return ;
+}
+void print(struct ListNode* head) {
+	struct ListNode* p;
+	for (p = head; p != NULL; p = p->next) {
+		printf("%d->", p->val);
+	}
+	printf("null\n");
+}
+int main() {
+	struct ListNode* list = (struct ListNode*)malloc(sizeof(struct ListNode));
+	list->next = NULL;
+	list->val = 0;
+ 	push(list, 1);
+	push(list, 2);
+	push(list, 3);
+	push(list, 4);
+	print(list);
+	print(reverseList(list));
+
+	system("pause");
+	return 0;
+}
