7a4fd878ee8ee31c9c0146b2ed17caf2f9891e57
Tony Liu <tonythomascn@gmail.com>
2015-07-02 09:48:00 -0400

    Matrix Zigzag Traversal, the direction of eventh and oddth lane is opposite.

--- lintcode/matrixzigzagtraversal.cpp
+++ lintcode/matrixzigzagtraversal.cpp
@@ -17,11 +17,27 @@ return [1, 2, 5, 9, 6, 3, 4, 7, 10, 11, 8, 12]
 */
 #include "stdafx.h"
 #include <vector>
+#include <algorithm>
 using std::vector;
 /**
 * @param matrix: a matrix of integers
 * @return: a vector of integers
 */
 vector<int> printZMatrix(vector<vector<int> > &matrix) {
-	// write your code here
+	vector<int> zigzag;
+	const int m = matrix.size(), n = matrix[0].size();
+
+	for (int i = 0; i < m + n - 1; i++) {
+		if (0 == i % 2) {
+			//on eventh lane, go from right to left
+			for (int y = std::max(0, i - m + 1); y <= std::min(i, n - 1); y++)
+				zigzag.emplace_back(matrix[i - y][y]);
+		}
+		else {
+			//on oddth lane, go from left to right
+			for (int x = std::max(0, i - n + 1); x <= std::min(i, m - 1); x++) 
+				zigzag.emplace_back(matrix[x][i - x]);
+		}
+	}
+	return zigzag;
 }
