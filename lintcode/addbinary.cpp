3e9139452846bddf83383b80846a293a45617cf8
Tony Liu <tonythomascn@gmail.com>
2015-07-16 12:54:08 -0400

    Another way to add binary, iterate every digit in both strings.

--- lintcode/addbinary.cpp
+++ lintcode/addbinary.cpp
@@ -18,14 +18,30 @@ Return 100
 #include <bitset>
 using std::string;
 string addBinary(string& a, string& b) {
-	// Write your code here
 	int binarya = std::stoi(a, 0, 2);
 	int binaryb = std::stoi(b, 0, 2);
 	if (!(binarya + binaryb)) return "0";
+	//access two binary integers and transform it into a string usn g bitset
 	string s = std::bitset<8>(binarya + binaryb).to_string();
 	return s.substr(s.find("1"));
 }
 
+string addBinary2(string&a, string&b) {
+	int carry = 0;
+	string result = "";
+	int ai, bj, val;
+	//iterate every digit in both string and add carry with them
+	for (int i = a.size() - 1, j = b.size() - 1; i >= 0 || j >= 0; i--, j--) {
+		ai = i >= 0 ? a[i] - '0' : 0;
+		bj = j >= 0 ? b[j] - '0' : 0;
+		val = (ai + bj + carry) % 2;
+		carry = (ai + bj + carry) / 2;
+		result.insert(result.begin(), val + '0');
+	}
+	if (carry)
+		result.insert(result.begin(), '1');
+	return result;
+}
 //int main()
 //{
 //	string a = "0";
