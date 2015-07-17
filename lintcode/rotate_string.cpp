0df8eb6c5bc810266dc264e77590bb507cccef8a
Tony Liu <tonythomascn@gmail.com>
2015-07-17 15:40:43 -0400

    Rotate string, offset could be any number, reverse the left string, right string and the whole string to complete the rotation.

---
+++ lintcode/rotate_string.cpp
@@ -0,0 +1,44 @@
+/*
+Rotate String
+
+22% Accepted
+Given a string and an offset, rotate string by offset. (rotate from left to right)
+
+Have you met this question in a real interview? Yes
+Example
+Given "abcdefg".
+
+offset=0 => "abcdefg"
+offset=1 => "gabcdef"
+offset=2 => "fgabcde"
+offset=3 => "efgabcd"
+Challenge
+Rotate in-place with O(1) extra memory.
+*/
+#include "stdafx.h"
+#include <string>
+using std::string;
+void reverse(string &s, int start, int end) {
+	unsigned char c;
+	while (start < end) {
+		c = s[start];
+		s[start] = s[end];
+		s[end] = c;
+		start++;
+		end--;
+	}
+}
+/**
+* param A: A string
+* param offset: Rotate string with offset.
+* return: Rotated string.
+*/
+string rotateString(string A, int offset) {
+	if (A.empty())
+		return A;
+	offset = offset % A.size();
+	reverse(A, 0, A.size() - offset - 1);
+	reverse(A, A.size() - offset, A.size() - 1);
+	reverse(A, 0, A.size() - 1);
+	return A;
+}
